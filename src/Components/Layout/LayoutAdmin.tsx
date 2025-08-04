65dfe6e3233d3634b434d7dc88a678f32b9377ad
Andre <andre.ats.profissional@gmail.com>
2025-08-04 01:50:10 -0300

    colocando menu navegacao para mobile responsivo

--- src/Components/Layout/LayoutAdmin.tsx
+++ src/Components/Layout/LayoutAdmin.tsx
@@ -1,4 +1,4 @@
-import { InfoIcon, X } from "lucide-react";
+import { InfoIcon, Menu, X } from "lucide-react";
 import { ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
@@ -17,9 +17,10 @@ export function LayoutAdmin(props: ILayoutAdmin) {
 
     const [mostrarIcon, setMostrarIcon] = useState(true)
     const [animacaoClasse, setAnimacaoClasse] = useState('');
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
-    const [linkHeader,] = useState(["/admin/dashboard", "/admin/itens/listagem", "/admin/pedidos/listagem"])
-    const [header,] = useState(["Dashboard", "Itens", "Pedidos"])
+    const [linkHeader,] = useState(["/admin/dashboard", "/admin/itens/listagem", "/admin/pedidos/listagem", "/login"])
+    const [header,] = useState(["Dashboard", "Itens", "Pedidos", "Sair"])
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -38,10 +39,23 @@ export function LayoutAdmin(props: ILayoutAdmin) {
                 <div className="w-4/5">
                     {props.header &&
                         <header className="w-full flex justify-between items-center h-20 px-10 bg-zinc-800 rounded-b-2xl">
-                            <div className="text-white">
+                            <div className="text-white flex items-center gap-4">
                                 <img onClick={() => navigate("/admin/dashboard")} className="w-[70px] cursor-pointer" src={logoRefeicoes} alt="Logo" />
+                                <p className="hidden lg:block">Admin Refeições Marmitas Congeladas</p>
                             </div>
-                            <div className="text-white">
+                            <button
+                                type="button"
+                                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                                className="block rounded-sm bg-zinc-800 hover:bg-zinc-700 p-2.5 text-white transition lg:hidden"
+                            >
+                                <span className="sr-only">Toggle menu</span>
+                                {isMobileMenuOpen ? (
+                                    <X className="size-5" />
+                                ) : (
+                                    <Menu className="size-5" />
+                                )}
+                            </button>
+                            <div className="text-white hidden lg:block">
                                 {props.header && (
                                     <div className="flex gap-6">
                                         {header?.map((item, index) => (
@@ -54,6 +68,21 @@ export function LayoutAdmin(props: ILayoutAdmin) {
                             </div>
                         </header>
                     }
+                    <div className="w-full flex justify-end">
+                        {isMobileMenuOpen && (
+                            <div className="bg-zinc-800 mt-1 rounded-md pr-10 pl-3 py-3 lg:hidden">
+                                {props.header && (
+                                    <div className="block">
+                                        {header?.map((item, index) => (
+                                            <div key={index} className="cursor-pointer mb-2">
+                                                <p className="text-white" onClick={() => navigate(linkHeader[index])}>{item}</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </div>
                     {props.infoPagina && mostrarIcon && (
                         <div
                             className="flex justify-between items-center bg-blue-400 rounded-2xl py-2 px-8 mt-5 bg-opacity-90 shadow-lg"
@@ -69,7 +98,7 @@ export function LayoutAdmin(props: ILayoutAdmin) {
                                     {props.infoPaginaTexto}
                                 </p>
                             </div>
-                            <X className="cursor-pointer" onClick={() => setMostrarIcon(false)} color="white"/>
+                            <X className="cursor-pointer" onClick={() => setMostrarIcon(false)} color="white" />
                         </div>
                     )}
                     {props.children}
